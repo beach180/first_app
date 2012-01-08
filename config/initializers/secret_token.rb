71cd647a55f7a0dac81bf0cd0d2881c53b6ee5b8
imacuser <imac@xiocom-techs-computer.local>
2012-01-08 10:11:48 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'afacdd756602e98910a698c2a4de17585af0e183835ac6d023ef0ab4e918f1e850a86f6709077ab717833a7887f3e5e0b5963e9ead6a71461ddfc1e4d253a854'
